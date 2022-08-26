7ce107e240c1a7dfd745efea95f6d02aa2eba666
fortmarek <marekfort@me.com>
2022-08-26 09:49:33 +0200

    Create Twitter with FlatList example

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Twitter} from './twitter';
+
+const App = () => {
+  return <Twitter />;
+};
+
+export default App;
---
+++ src/twitter/TweetCell.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Pressable} from 'react-native';
+
+import Tweet from './models/Tweet';
+import TweetContent from './TweetContent';
+
+export interface TweetCellProps {
+  tweet: Tweet;
+}
+
+const TweetCell = ({tweet}: TweetCellProps) => {
+  return (
+    <Pressable
+      onPress={() => {
+        console.log('Pressed tweet', tweet);
+      }}>
+      <TweetContent tweet={tweet} />
+    </Pressable>
+  );
+};
+
+export default TweetCell;
