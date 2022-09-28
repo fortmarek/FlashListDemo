40f1d51874f64443fcbc0216bf934e73959e517e
fortmarek <marekfort@me.com>
2022-09-28 12:14:09 +0200

    Expo init

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: './src',
-        cwd: 'babelrc',
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        alias: {
-          assets: './src/assets',
-        },
-      },
-    ],
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {Twitter} from './twitter';
-
-const App = () => {
-  return <Twitter />;
-};
-
-export default App;
